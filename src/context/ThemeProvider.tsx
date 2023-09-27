acfbef23d4634e5e032cc47290cf29d7608eb3d9
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-09-27 23:17:15 +0300

    added navbar

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,42 +1,46 @@
-"use client"
-import React, {createContext, useContext, useState, useEffect} from "react"
+"use client";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
-    mode : string,
-    setMode : (mode : string) => void,
+  mode: string;
+  setMode: (mode: string) => void;
 }
 
-const ThemeContext = createContext <ThemeContextType | undefined> (undefined);
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
-export function ThemeProvider({children} : {children:React.ReactNode}){
-    const [mode, setMode] = useState('');
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("light");
 
-    const handleThemeChange = () => {
-            if (mode === 'dark'){
-                setMode('light');
-                document.documentElement.classList.add('light');
-            }else{
-                setMode('dark');
-                document.documentElement.classList.add('dark');
-            }
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme:dark)").matches)
+    ) {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add("light");
     }
+  };
 
-    useEffect(() => {
-      handleThemeChange();
-    }, [mode])
-    
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
 
-    return (
-        <ThemeContext.Provider value={{mode, setMode}}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-export function useTheme(){
-    const context = useContext(ThemeContext);
-    if (context === undefined){
-        throw new Error("useTheme must be used within a ThemeProvider")
-    }
-    return context;
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
 }
