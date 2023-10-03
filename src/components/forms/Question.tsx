d5e42c4f9efbab9b3d88201f2021e3dfd6fb29df
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-10-03 13:17:01 +0300

    created a question form

--- src/components/forms/Question.tsx
+++ src/components/forms/Question.tsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef. useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -17,8 +17,12 @@ import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validation";
 import * as z from "zod";
 import { Editor } from "@tinymce/tinymce-react";
+import { Badge } from "../ui/badge";
+
+const type:any = 'create';
 
 const Question = () => {
+  const [isSubmiting, setIsSubmiting] = useState(false);
   const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
@@ -36,7 +40,52 @@ const Question = () => {
   });
 
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
-    console.log(values);
+    setIsSubmiting(true);
+    try {
+      if (type === 'create'){
+
+      }else{
+
+      }
+
+      // navigate to home
+      
+    } catch (error) {
+      
+    }finally{
+      setIsSubmiting(false)
+    }
+  }
+
+  function handleInputKeyDown(
+    e: React.KeyboardEvent<HTMLInputElement>,
+    field: any
+  ) {
+    if (e.key === "Enter" && field.name === "tags") {
+      e.preventDefault();
+      const tagInput = e.target as HTMLInputElement;
+      const tagValue = tagInput.value.trim();
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters.",
+          });
+        }
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue("tags", [...field.value, tagValue]);
+          tagInput.value = "";
+          form.clearErrors("tags");
+        }
+      } else {
+        form.trigger();
+      }
+    }
+  }
+
+  function handleTagRemove(tag:string, field:any){
+    const newTags = field.value.filter((t:string) => t !== tag);
+    form.setValue('tags', newTags);
   }
 
   return (
@@ -80,23 +129,39 @@ const Question = () => {
               <FormControl className="mt-3.5">
                 {/* Editor  */}
                 <Editor
-                  onInit={(evt, editor) => (editorRef.current = editor)}
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                  onInit={(evt, editor) => {
+                    // @ts-ignore
+                    editorRef.current = editor;
+                  }}
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
                   initialValue=""
                   init={{
-                    height: 500,
+                    height: 350,
                     menubar: false,
                     plugins: [
-                      "advlist autolink lists link image charmap print preview anchor",
-                      "searchreplace visualblocks code fullscreen",
-                      "insertdatetime media table paste code help wordcount",
+                      "advlist",
+                      "autolink",
+                      "lists",
+                      "link",
+                      "image",
+                      "charmap",
+                      "preview",
+                      "anchor",
+                      "searchreplace",
+                      "visualblocks",
+                      "codesample",
+                      "fullscreen",
+                      "insertdatetime",
+                      "media",
+                      "table",
                     ],
                     toolbar:
-                      "undo redo | formatselect | " +
-                      "bold italic backcolor | alignleft aligncenter " +
-                      "alignright alignjustify | bullist numlist outdent indent | " +
-                      "removeformat | help",
-                    content_style:
-                      "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                      "undo redo | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter |" +
+                      "alignright alignjustify | bullist numlist",
+                    content_style: "body { font-family:Inter; font-size:16px }",
                   }}
                 />
               </FormControl>
@@ -119,11 +184,33 @@ const Question = () => {
                 <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  {...field}
-                  placeholder="Add tags..."
-                  className="no-focus paragrah-regular background-light900_dark300 light-border-2 min-h-[56px] border"
-                />
+                <>
+                  <Input
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                    placeholder="Add tags..."
+                    className="no-focus paragrah-regular background-light900_dark300 light-border-2 min-h-[56px] border"
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag, idx) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image
+                            src="/assets/icons/close.svg"
+                            alt="Close icon"
+                            width={12}
+                            height={12}
+                            className="cursor-pointer object-contain invert-0 dark:invert"
+                          />
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 {" "}
@@ -134,7 +221,23 @@ const Question = () => {
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+        <Button type="submit" className="primary-gradient text-white w-fit" disabled={isSubmiting}>
+          {
+            isSubmiting?(
+              <>
+                {
+                  type === 'edit' ? 'Editing...' : 'Posting...'
+                }
+              </>
+            ):(
+              <>
+                {
+                  type === 'edit' ? 'Edit Question' : 'Ask a Question'
+                }
+              </>
+            )
+          }
+        </Button>
       </form>
     </Form>
   );
