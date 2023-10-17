c0fad8043935110c0e835febff9c61ab2371e9ac
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-10-17 17:38:37 +0300

    connected to db

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,7 +1,9 @@
 import Question from '@/components/forms/Question'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { createQuestion } from '@/lib/actions/question.action'
 
 const AskQuestion = () => {
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
--- src/lib/actions/question.action.ts
+++ src/lib/actions/question.action.ts
@@ -1,8 +1,10 @@
 "use server"
+import { connectToDatabase } from "../mongoose"
 
 export async function createQuestion(params:object){
     try {
         // 1.connect to DB
+        connectToDatabase();
         
     } catch (error) {
         console.log(error)
