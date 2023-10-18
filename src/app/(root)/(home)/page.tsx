39b36e2e4234eff7605756da9fa73621bf96282c
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-10-18 15:24:36 +0300

    fix errors 3

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -9,24 +9,14 @@ import NoResult from "@/components/shared/NoResult";
 import QuestionCard from "@/components/cards/QuestionCard";
 import { getQuestions } from "@/lib/actions/question.action";
 
-
-
-
-
-
-
 export default async function Home() {
   const result = await getQuestions({});
-
 
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-3 sm:flex-row sm:items-center ">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-        <Link
-          href={"/ask-question"}
-          className="flex justify-end max-sm:w-full"
-        >
+        <Link href={"/ask-question"} className="flex justify-end max-sm:w-full">
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Question
           </Button>
@@ -41,7 +31,7 @@ export default async function Home() {
           placeholder="Search for questions"
           otherClases="flex-1"
         />
-         <Filter
+        <Filter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
@@ -50,25 +40,31 @@ export default async function Home() {
       <HomeFilters filters={HomePageFilters} />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-          
         {
-          result===undefined?(<NoResult title="There's no questions to show" description=" Be the first to break the silence! 🚀 Ask a Question and kickstart the
-          discussion. our query could be the next big thing others learn from. Get
-          involved! 💡" link='/ask-question' linkTitle='Ask a Question'/>):(
-            result.questions.length>0?result.questions.map((question, idx) => (
-              <QuestionCard key={idx}  _id={question._id}
-              title={question.title}
-              tags={question.tags}
-              author={question.author}
-              upvotes={question.upvotes}
-              views={question.views}
-              answers={question.answers}
-              createdAt={question.createdAt}/>)
-          )
+          result === undefined ? (
+            <NoResult
+              title="There's no questions to show"
+              description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. Your query could be the next big thing others learn from. Get involved! 💡"
+              link="/ask-question"
+              linkTitle="Ask a Question"
+            />
+          ) : result.questions.length > 0 ? (
+            result.questions.map((question, idx) => (
+              <QuestionCard
+                key={idx}
+                _id={question._id}
+                title={question.title}
+                tags={question.tags}
+                author={question.author}
+                upvotes={question.upvotes}
+                views={question.views}
+                answers={question.answers}
+                createdAt={question.createdAt}
+              />
+            ))
+          ) : null 
         }
       </div>
     </>
   );
 }
-
-
